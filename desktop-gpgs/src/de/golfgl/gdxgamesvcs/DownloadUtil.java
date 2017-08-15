2f2a77f24317c969b2b531858d791f7e9a474f6a
Benjamin Schulte <lightblocks@golfgl.de>
2017-08-15 21:49:14 +0200

    IntelliJ automatic code formatting

--- desktop-gpgs/src/de/golfgl/gdxgamesvcs/DownloadUtil.java
+++ desktop-gpgs/src/de/golfgl/gdxgamesvcs/DownloadUtil.java
@@ -1,35 +1,35 @@
 package de.golfgl.gdxgamesvcs;
 
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.utils.StreamUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.utils.StreamUtils;
-
 public class DownloadUtil {
 
-	public static Pixmap downloadImage(String imageUrl) throws IOException{
-		byte[] bytes = download(imageUrl);
-		return new Pixmap(bytes, 0, bytes.length);
-	}
-	
-	public static byte[] download(String url) throws IOException {
-		InputStream in = null;
-		try {
-			HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
-			conn.setDoInput(true);
-			conn.setDoOutput(false);
-			conn.setUseCaches(true);
-			conn.connect();
-			in = conn.getInputStream();
-			return StreamUtils.copyStreamToByteArray(in);
-		} catch (IOException ex) {
-			throw ex;
-		} finally {
-			StreamUtils.closeQuietly(in);
-		}
-	}
+    public static Pixmap downloadImage(String imageUrl) throws IOException {
+        byte[] bytes = download(imageUrl);
+        return new Pixmap(bytes, 0, bytes.length);
+    }
+
+    public static byte[] download(String url) throws IOException {
+        InputStream in = null;
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setDoInput(true);
+            conn.setDoOutput(false);
+            conn.setUseCaches(true);
+            conn.connect();
+            in = conn.getInputStream();
+            return StreamUtils.copyStreamToByteArray(in);
+        } catch (IOException ex) {
+            throw ex;
+        } finally {
+            StreamUtils.closeQuietly(in);
+        }
+    }
 
 }
