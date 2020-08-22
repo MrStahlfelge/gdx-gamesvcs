29fb9c4b57b697ed1879b6ec2a992441b651c193
francescostranieri <fer.stranieri@gmail.com>
2020-08-22 11:25:34 +0200

    new auth and game service versions; leaderboards switch status

---
+++ android-huawei/src/de/golfgl/gdxgamesvcs/HuaweiGameServicesConstants.java
@@ -0,0 +1,6 @@
+package de.golfgl.gdxgamesvcs;
+
+public class HuaweiGameServicesConstants {
+    public static final int HUAWEI_GAMESVCS_LEADERBOARDS_ENABLED = 1;
+    public static final int HUAWEI_GAMESVCS_LEADERBOARDS_DISABLED = 0;
+}
